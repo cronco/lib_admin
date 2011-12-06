6ce97a2d00dae8b33098aef4bbea3b6f44f31224
Mihai Chereji <mihai.chereji@gmail.com>
2011-12-06 10:49:50 +0200

    Added menu to views, basic style, added news to home view

--- views.py
+++ views.py
@@ -1,11 +1,18 @@
 from django.http import HttpResponse
 from django.template import Context, loader
 from django.shortcuts import render_to_response
-from la.models import Book, Library
+from la.models import Book, Library, News
+import datetime
+
+l = Library.objects.get()
+c = Context({"library" : l})
 
 def home(request):
-	return HttpResponse('You are looking at the homepage of something awesome')
+	news = News.objects.all().order_by('-pub_date')[:5]
+	c['news'] = news
+	return render_to_response('lib_admin/home.html', c)
 
 def book(request, book_id):
 	b = Book.objects.get(pk = book_id)
-	return render_to_response('lib_admin/book.html', { 'book' : b})
+	c['book'] = b
+	return render_to_response('lib_admin/book.html', c)
