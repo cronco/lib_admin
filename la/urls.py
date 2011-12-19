ef0b361e9c2b239228ee8a856e5ae4c85e5567bc
Mihai Chereji <mihai.chereji@gmail.com>
2011-12-19 05:36:49 +0200

    Further refinement to checkouts.html and urls.py

--- la/urls.py
+++ la/urls.py
@@ -31,11 +31,8 @@ urlpatterns = patterns('la.views',
 		# Finally, admin stuff
 		url(r'^admins/?$', 'admin', name='admin'),
 
-    # url(r'^lib_admin/', include('lib_admin.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
-    # Uncomment the next line to enable the admin:
-  #   url(r'^admin/', include(admin.site.urls)),
 )
